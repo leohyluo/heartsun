f5de58451d4a3529b79ae0be0752c7353cf9c2b0
leohyluo <32305819@qq.com>
2017-02-08 18:55:17 +0800

    update rest template

--- ssh/src/test/java/com/heartsun/controller/MainSymptomControllerTest.java
+++ ssh/src/test/java/com/heartsun/controller/MainSymptomControllerTest.java
@@ -1,5 +1,7 @@
 package com.heartsun.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
@@ -21,9 +23,32 @@ public class MainSymptomControllerTest {
 	
 	@Test
 	public void testQuery() {
-		String uri = BASE_URL + "/query";
-		ResponseEntity<MainSymptom> response = restTemplate.postForEntity(uri, null, MainSymptom.class);
-		response.getBody();
-		System.out.println("end");
+		try {
+			String uri = BASE_URL + "/query";
+			//ResponseEntity<MainSymptom> response = restTemplate.postForEntity(uri, null, MainSymptom.class);
+			ResponseEntity<List> response = restTemplate.postForEntity(uri, null, List.class);
+			List list = response.getBody();
+			System.out.println("size=="+list.size());
+			for(int i = 0; i<list.size(); i++) {
+				MainSymptom m = (MainSymptom) list.get(i);
+				System.out.println("mobile="+m.getName());
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testQuery2() {
+		try {
+			String uri = BASE_URL + "/query";
+			ResponseEntity<String> response = restTemplate.postForEntity(uri, null, String.class);
+			System.out.println(response.getBody());
+			System.out.println("end");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
 }
