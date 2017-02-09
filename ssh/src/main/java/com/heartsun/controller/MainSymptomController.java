910133dd364ce244a22322d816b3798e06b2ad22
leohyluo <32305819@qq.com>
2017-02-09 18:47:00 +0800

    update log4j

--- ssh/src/main/java/com/heartsun/controller/MainSymptomController.java
+++ ssh/src/main/java/com/heartsun/controller/MainSymptomController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +17,8 @@ import com.heartsun.service.IMainSymptomService;
 @Controller
 @RequestMapping("/mainSymptom")
 public class MainSymptomController extends AbstractController {
+	
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Resource
 	private IMainSymptomService mainSymptomService;
@@ -32,8 +36,7 @@ public class MainSymptomController extends AbstractController {
 	public List<MainSymptom> query(MainSymptom m) throws Exception {
 		List<MainSymptom> list = null;
 		list = mainSymptomService.get(m);
-		System.out.println("list.size="+list.size());
-		System.out.println(1/0);
+		logger.info("MainSymptom size = " + list.size());
 		return list;
 	}
 	
